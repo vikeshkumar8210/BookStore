6819711e52dc0a587065eef9cbabac3646e81d11
vikesh <vikeshkr2ins@gmail.com>
2024-01-24 11:16:58 +0530

    first commit

---
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,52 @@
+package com.example.bookstore.controller;
+
+
+import com.example.bookstore.Book;
+import com.example.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    // Endpoint for getting all books
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    // Endpoint for getting a specific book by ID
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id).orElse(null);
+    }
+
+    // Endpoint for adding a new book
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.saveBook(book);
+    }
+
+    // Endpoint for updating an existing book
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        // Set the ID to ensure the correct book is updated
+        book.setId(id);
+        return bookService.saveBook(book);
+    }
+
+    // Endpoint for deleting a book by ID
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
+}
---
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -0,0 +1,39 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.Book;
+import com.example.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    // Methods of book Management
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+
+}
